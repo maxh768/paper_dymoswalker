c5a8abe0a3b6c12c2be8f680605aad694b747d6f
maxh768 <mhowel30@vols.utk.edu>
2024-02-10 16:57:45 -0500

    fixed partials compass gait

--- code/compass-gait/compassdynamics.py
+++ code/compass-gait/compassdynamics.py
@@ -1,11 +1,9 @@
-""" dynamics of a legged walker robot"""
-
 import numpy as np
 import openmdao.api as om
 from numpy.linalg import inv
 from numpy.linalg import multi_dot
 
-class kneedWalker(om.Group):
+class system(om.Group):
 
     def initialize(self):
         self.options.declare('num_nodes', types=int)
@@ -14,13 +12,12 @@ class kneedWalker(om.Group):
     def setup(self):
         nn=self.options['num_nodes']
 
-        input_names = ['L', 'a1', 'b1', 'a2', 'b2', 'x1', 'x2', 'x3', 'x4', 'm_H', 'm_t', 'm_s', 'tau']
-        self.add_subsystem('lockedknee', lockedKneeDynamics(num_nodes=nn, ), promotes_inputs=input_names, promotes_outputs=['*'])
+        input_names = ['a', 'b', 'x1', 'x2', 'x3', 'x4', 'mh', 'm', 'tau']
+        self.add_subsystem('lockedknee', dynamics(num_nodes=nn, ), promotes_inputs=input_names, promotes_outputs=['*'])
         self.add_subsystem('cost', CostFunc(num_nodes=nn, states_ref=self.options['states_ref'] ), promotes_inputs=['x1', 'x2'], promotes_outputs=['*'])
 
 
-
-class lockedKneeDynamics(om.ExplicitComponent):
+class dynamics(om.ExplicitComponent):
     
     def initialize(self):
         self.options.declare('num_nodes', types=int)
@@ -31,14 +28,10 @@ class lockedKneeDynamics(om.ExplicitComponent):
         nn = self.options['num_nodes']
 
         # inputs
-        self.add_input('L', shape=(1,),units='m',desc="length of one leg") 
 
         # length paramters
-        self.add_input('a1',shape=(1,),units='m',desc='shank length below point mass')
-        self.add_input('b1', shape=(1,),units='m', desc='shank length above point mass')
-
-        self.add_input('a2', shape=(1,),units='m', desc='thigh length below points mass')
-        self.add_input('b2', shape=(1,),units='m', desc='thigh length above point mass')
+        self.add_input('a',shape=(1,),units='m')
+        self.add_input('b', shape=(1,),units='m')
 
         """
         x1 = q1, x2 = q2, x3 = q1_dot, x4 = q2_dot
@@ -49,9 +42,9 @@ class lockedKneeDynamics(om.ExplicitComponent):
         self.add_input('x4', shape=(nn,),units='rad/s', desc='q2 dot')
 
         # masses
-        self.add_input('m_H', shape=(1,),units='kg', desc='hip mass')
-        self.add_input('m_t', shape=(1,),units='kg', desc='thigh mass')
-        self.add_input('m_s', shape=(1,),units='kg', desc='shank mass')
+        self.add_input('m', shape=(1,),units='kg')
+        self.add_input('mh', shape=(1,),units='kg')
+
 
         # applied torque - torque is applied equally and opposite to each leg
         self.add_input('tau', shape=(nn,),units='N*m', desc='applied toruqe at hip')
@@ -67,7 +60,7 @@ class lockedKneeDynamics(om.ExplicitComponent):
         self.add_output('x4_dot', shape=(nn,), units='rad/s**2',desc='q2 dotdot')
 
         #partials
-        #self.declare_partials(of=['*'], wrt=['x1', 'x2', 'x3', 'x4', 'tau'], method='cs')#, rows=np.arange(nn), cols=np.arange(nn))
+        self.declare_partials(of=['*'], wrt=['x1', 'x2', 'x3', 'x4', 'tau'], method='exact', rows=np.arange(nn), cols=np.arange(nn))
         #self.declare_coloring(wrt=['x1', 'x2', 'x3', 'x4'], method='cs', show_summary=False)
         #self.set_check_partial_options(wrt=['x1', 'x2', 'x3', 'x4'], method='fd', step=1e-6)
 
@@ -77,90 +70,85 @@ class lockedKneeDynamics(om.ExplicitComponent):
 
     def compute(self, inputs, outputs):
         g = self.options['g']
-        L = inputs['L']
-        a1 = inputs['a1']
-        a2 = inputs['a2']
-        b1 = inputs['b1']
-        b2 = inputs['b2']
-        m_H = inputs['m_H']
-        m_t = inputs['m_t']
-        m_s = inputs['m_s']
+        a = inputs['a']
+        b = inputs['b']
+        mh = inputs['mh']
+        m = inputs['m']
         x1 = inputs['x1']
         x2 = inputs['x2']
         x3 = inputs['x3']
         x4 = inputs['x4']
         tau = inputs['tau']
 
-        ls = a1 + b1
-        lt = a2 + b2
+        l = a + b
 
+        # q1 = Ost = x1, q2 = Osw = x2
         # mtrix components
-        H11 = m_s*a1**2 + m_t*(ls + a2)**2 + (m_H + m_s + m_t)*(L**2)
-        H12 = -(m_t*b2 + m_s*(lt + b1))*(L*np.cos(x2-x1)) #
-        H22 = m_t*b2**2 + m_s*(lt + b1)**2
-        h = -(m_t*b2 + m_s*(lt + b1))*(L*np.sin(x1-x2)) #
-        G1 = -(m_s*a1 + m_t*(ls+a2) + (m_H + m_t + m_s)*L)*g*np.sin(x1) #
-        G2 = (m_t*b2 + m_s*(lt + b1))*g*np.sin(x2) #
+        H11 = (mh + m)*(l**2) + m*a**2
+        H12 = -m*l*b*np.cos(x1 - x2)
+        H22 = m*b**2
+        h = m*l*b*np.sin(x1-x2)
+        G1 = (mh*l + m*a + m*l)*g*np.sin(x1)
+        G2 = -m*b*g*np.sin(x2)
 
-        K = 1 / (H11*H22 - H12*H12) # inverse constant
+        K = 1 / (H11*H22 - H12**2) # inverse constant
 
         outputs['x1_dot'] = x3
         outputs['x2_dot'] = x4
-        outputs['x3_dot'] = H12*K*h*x3 + H22*K*h*x4 - (H22*K*G1 - H12*K*G2) + ((-H22*K - H12*K)*tau)
-        outputs['x4_dot'] = -H11*K*h*x3 - H12*K*h*x4 - (-H12*K*G1 + H11*K*G2) + ((H12*K + H11*K)*tau)
+        outputs['x3_dot'] = (H12*K*h*x3**2) + (H22*K*h*x4**2) - (H22 + H12)*K*tau - H22*K*G1 + H12*K*G2
+        outputs['x4_dot'] = (-H11*K*h*x3**2) - (H12*K*h*x4**2) + ((H12 + H11)*K*tau) + H12*K*G1 - H11*K*G1
 
     def compute_partials(self, inputs, partials):
-       """ # computes analytical partials 
+       # computes analytical partials 
 
         g = self.options['g']
-        L = inputs['L']
-        a1 = inputs['a1']
-        a2 = inputs['a2']
-        b1 = inputs['b1']
-        b2 = inputs['b2']
-        m_H = inputs['m_H']
-        m_t = inputs['m_t']
-        m_s = inputs['m_s']
+        a = inputs['a']
+        b = inputs['b']
+        mh = inputs['mh']
+        m = inputs['m']
         x1 = inputs['x1']
         x2 = inputs['x2']
         x3 = inputs['x3']
         x4 = inputs['x4']
         tau = inputs['tau']
 
-        ls = a1 + b1
-        lt = a2 + b2
+        l = a + b
 
-        H11 = m_s*a1**2 + m_t*(ls + a2)**2 + (m_H + m_s + m_t)*(L**2)
-        H12 = -(m_t*b2 + m_s*(lt + b1))*(L*np.cos(x2-x1)) #
-        H22 = m_t*b2**2 + m_s*(lt + b1)**2
-        h = -(m_t*b2 + m_s*(lt + b1))*(L*np.sin(x1-x2)) #
-        G1 = -(m_s*a1 + m_t*(ls+a2) + (m_H + m_t + m_s)*L)*g*np.sin(x1) #
-        G2 = (m_t*b2 + m_s*(lt + b1))*g*np.sin(x2)
+        # q1 = Ost = x1, q2 = Osw = x2
+        # mtrix components
+        H11 = (mh + m)*(l**2) + m*a**2
+        H12 = -m*l*b*np.cos(x1 - x2)
+        H22 = m*b**2
+        h = m*l*b*np.sin(x1-x2)
+        G1 = (mh*l + m*a + m*l)*g*np.sin(x1)
+        G2 = -m*b*g*np.sin(x2)
 
-        K = 1 / (H11*H22 - H12*H12)
+        K = 1 / (H11*H22 - H12**2) # inverse constant
 
         # partials of terms wrt q1 q2
-        H12_dq1 = -(m_t*b2 + m_s*(lt + b1))*L*np.sin(x2-x1)
-        H12_dq2 = -(m_t*b2 + m_s*(lt + b1))*L*np.sin(x2-x1)*(-1)
-        h_dq1 = -(m_t*b2 + m_s*(lt + b1))*(L*np.cos(x1-x2))
-        h_dq2 = -(m_t*b2 + m_s*(lt + b1))*(L*np.cos(x1-x2))*(-1)
-        G1_dq1 = -(m_s*a1 + m_t*(ls+a2) + (m_H + m_t + m_s)*L)*g*np.cos(x1)
-        G2_dq2 =  (m_t*b2 + m_s*(lt + b1))*g*np.cos(x2)
+        H12_dq1 = m*l*b*np.sin(x1-x2)
+        H12_dq2 = -m*l*b*np.sin(x1-x2)
+        h_dq1 = m*l*b*np.cos(x1-x2)
+        h_dq2 = -m*l*b*np.cos(x1-x2)
+        G1_dq1 = (mh*l + m*a + m*l)*g*np.cos(x1)
+        G2_dq2 = -m*b*g*np.cos(x2)
 
-        K_dq1 = (-(H11*H12 - H12**2)**(-2))*(-2*H12)*(H12_dq1)
-        K_dq2 = (-(H11*H12 - H12**2)**(-2))*(-2*H12)*(H12_dq2)     
+        H12_abs = -H12
 
-        partials['x3_dot', 'x1'] = (H12_dq1*K*h*x3 + H12*K_dq1*h*x3 + h_dq1*H12*K*x3) + (H22*K_dq1*h*x4 + H22*x4*K*h_dq1) - (H22*K_dq1*G1 + H22*K*G1_dq1) + (H12_dq1*K*G2 + H12*K_dq1*G2) - (H22*K_dq1*tau) - (H12*K_dq1 + H12_dq1*K)*tau
-        partials['x3_dot', 'x2'] = (H12_dq2*K*h*x3 + H12*K_dq2*h*x3 + H12*K*h_dq2*x3) + (H22*K_dq2*h*x4 + H22*x4*K*h_dq2) - (H22*K_dq2*G1) + (H12_dq2*K*G2 + H12*K_dq2*G2 + H12*K*G2_dq2) - (H22*K_dq2*tau) - (H12*K_dq2 + H12_dq2*K)*tau
-        partials['x3_dot', 'x3'] = H12*K*h
-        partials['x3_dot', 'x4'] = H22*K*h
-        partials['x3_dot', 'tau'] = -H22*K - H12*K
+        K_dq1 = (-(H11*H22 + H12_abs)**(-2))*(-2*H12_abs)*(-H12_dq1)
+        K_dq2 = (-(H11*H22 + H12_abs)**(-2))*(-2*H12_abs)*(-H12_dq2)
 
-        partials['x4_dot', 'x1'] = (-H11*K_dq1*h*x3 - H11*K*h_dq1*x3) - (H12_dq1*K*h*x4 + H12*K_dq1*h*x4 + H12*K*h_dq1*x4) + (H12_dq1*K*G1 + H12*K_dq1*G1 + H12*K*G1_dq1) - (H11*G2*K_dq1) + (H12_dq1*K + H12*K_dq1)*tau + (H11*K_dq1)*tau
-        partials['x4_dot', 'x2'] = (-H11*K_dq2*h*x3 - H11*K*h_dq2*x3) - (H12_dq2*K*h*x4 + H12*K_dq2*h*x4 + H12*K*h_dq2*x4) + (H12_dq2*K*G1 + H12*K_dq2*G1) - (H11*G2*K_dq2 + H11*G2_dq2*K) + (H12_dq2*K + H12*K_dq2)*tau + (H11*K_dq2)*tau
-        partials['x4_dot', 'x3'] = -H11*K*h
-        partials['x4_dot', 'x4'] = -H12*K*h
-        partials['x4_dot', 'tau'] = H12*K + H11*K
+        partials['x3_dot', 'x1'] = (x3**2)*((H12_dq1*K*h) + (H12*K_dq1*h) + (H12*K*h_dq1)) + ((x4**2)*H22)*((K_dq1*h) + (K*h_dq1)) - (H22*K_dq1*tau) - (tau*((H12_dq1*K) + (H12*K_dq1))) - (H22*((K_dq1*G1) + (K*G1_dq1))) + G2*((H12_dq1*K) + (H12*K_dq1))
+        partials['x3_dot', 'x2'] = (x3**2)*((H12_dq2*K*h) + (H12*K_dq2*h) + (H12*K*h_dq2)) + ((x4**2)*H22)*((K_dq2*h) + (K*h_dq2)) - (H22*K_dq2*tau) - (tau*((H12_dq2*K) + (H12*K_dq2))) - (H22*K_dq2*G1) + ((H12_dq2*K*G2) + (H12*K_dq2*G2) + (H12*K*G2_dq2))
+        partials['x3_dot', 'x3'] = 2*H12*K*h*x3
+        partials['x3_dot', 'x4'] = 2*H22*K*h*x4
+        partials['x3_dot', 'tau'] = -(H22 + H12)*K
+
+        partials['x4_dot', 'x1'] = (-x3**2)*H11*((K_dq1*h) + (K*h_dq1)) + (-x4**2)*((H12_dq1*K*h) + (H12*K_dq1*h) + (H12*K*h_dq1)) + tau*((H12_dq1*K) + (H12*K_dq1)) + (H11*tau*K_dq1) + ((H12_dq1*K*G1) + (H12*K_dq1*G1) + (H12*K*G1_dq1)) - (H11*K_dq1*G2)
+        partials['x4_dot', 'x2'] = (-x3**2)*H11*((K_dq2*h) + (K*h_dq2)) + (-x4**2)*((H12_dq2*K*h) + (H12*K_dq2*h) + (H12*K*h_dq2)) + tau*((H12_dq2*K) + (H12*K_dq2)) + (H11*tau*K_dq2) + G1*((H12_dq2*K) + (H12*K_dq2)) - (H11*((K_dq2*G2) + (K*G2_dq2)))
+        partials['x4_dot', 'x3'] = -2*H11*K*h*x3
+        partials['x4_dot', 'x4'] = -2*H12*K*h*x4
+        partials['x4_dot', 'tau'] = (H11 + H12)*K
         
         partials['x1_dot', 'x1'] = 0
         partials['x1_dot', 'x2'] = 0
@@ -170,7 +158,8 @@ class lockedKneeDynamics(om.ExplicitComponent):
         partials['x2_dot', 'x1'] = 0
         partials['x2_dot', 'x2'] = 0
         partials['x2_dot', 'x3'] = 0
-        partials['x2_dot', 'x4'] = 1 """
+        partials['x2_dot', 'x4'] = 1
+       
 
 class CostFunc(om.ExplicitComponent):
     # Computes the Cost
@@ -187,7 +176,7 @@ class CostFunc(om.ExplicitComponent):
 
         self.add_output('costrate', shape=(nn,), desc='quadratic cost rate')
         
-        #self.declare_partials(of=['costrate'], wrt=['x1', 'x2',], method='exact', rows=np.arange(nn), cols=np.arange(nn))
+        self.declare_partials(of=['costrate'], wrt=['x1', 'x2',], method='exact', rows=np.arange(nn), cols=np.arange(nn))
         #self.declare_coloring(wrt=['m_H','m_t','m_s', 'tau',], method='cs', show_summary=False)
         #self.set_check_partial_options(wrt=['m_H','m_t','m_s', 'tau',], method='fd', step=1e-6)
 
@@ -206,7 +195,7 @@ class CostFunc(om.ExplicitComponent):
 
         outputs['costrate'] = dx1**2 + dx2**2
 
-    """def compute_partials(self, inputs, partials,):
+    def compute_partials(self, inputs, partials,):
         #tau = inputs['tau']
         x1 = inputs['x1']
         x2 = inputs['x2']
@@ -218,18 +207,18 @@ class CostFunc(om.ExplicitComponent):
         # distance of current states from final states
         dx1 = x1 - x1ref
         dx2 = x2-x2ref
-
+        
     
         #partials['costrate', 'tau'] = 0
         partials['costrate', 'x1'] = 2*dx1
-        partials['costrate', 'x2'] = 2*dx2 """
+        partials['costrate', 'x2'] = 2*dx2
 
-        
+
 def check_partials():
     nn = 3
     states_ref = {'x1': 10*(np.pi / 180), 'x3': 0, 'x2': 20*(np.pi / 180), 'x4': 0}
     p = om.Problem()
-    p.model.add_subsystem('dynamics', kneedWalker(num_nodes=nn, states_ref=states_ref),promotes=['*'])
+    p.model.add_subsystem('dynamics', system(num_nodes=nn, states_ref=states_ref),promotes=['*'])
 
     """p.model.set_input_defaults('L', val=1, units='m')
     p.model.set_input_defaults('a1', val=0.375, units='m')
@@ -255,4 +244,4 @@ def check_partials():
 
 if __name__ == '__main__':
     check_partials()
-    
+
--- code/compass-gait/runcompass.py
+++ code/compass-gait/runcompass.py
@@ -1,7 +1,7 @@
 import numpy as np
 import openmdao.api as om
 import dymos as dm
-from leggeddynamics import kneedWalker
+from compassdynamics import system
 import matplotlib.pyplot as plt
 from dymos.examples.plotting import plot_results
 
@@ -10,14 +10,11 @@ def main():
     duration_lockphase = 10 # duration of locked knee phase
 
     # defining paramters of the legged walker
-    L = 1
-    a1 = 0.375
-    a2 = 0.175
-    b1 = 0.125
-    b2 = 0.325
-    m_H = 0.5
-    m_t = 0.5
-    m_s = 0.05
+
+    a = 0.375
+    b = 0.175
+    mh = 0.5
+    m = 1.2
 
     """ 
     walker will complete one full cycle -- states will be the same at the end as they were at the beginning (maybe ?)
@@ -34,7 +31,7 @@ def main():
 
     traj = p.model.add_subsystem('traj', dm.Trajectory())
 
-    lockphase = traj.add_phase('lockphase', dm.Phase(ode_class=kneedWalker, transcription=dm.GaussLobatto(num_segments=100, order=3), ode_init_kwargs={'states_ref': states_final}))
+    lockphase = traj.add_phase('lockphase', dm.Phase(ode_class=system, transcription=dm.GaussLobatto(num_segments=10, order=3), ode_init_kwargs={'states_ref': states_final}))
 
     lockphase.set_time_options(fix_initial=True, initial_val=0, fix_duration=True, duration_val=duration_lockphase, duration_ref=duration_lockphase, units='s') # set time of simulation    
 
@@ -48,14 +45,10 @@ def main():
     lockphase.add_control('tau', lower = 0, upper = 10, fix_initial=False, units='N*m') # add control torque
 
     # paramaters
-    lockphase.add_parameter('L', val=L, units='m', static_target=True)
-    lockphase.add_parameter('a1', val=a1, units='m', static_target=True)
-    lockphase.add_parameter('a2', val=a2, units='m', static_target=True)
-    lockphase.add_parameter('b1', val=b1, units='m', static_target=True)
-    lockphase.add_parameter('b2', val=b2, units='m', static_target=True)
-    lockphase.add_parameter('m_H', val=m_H, units='kg', static_target=True)
-    lockphase.add_parameter('m_t', val=m_t, units='kg', static_target=True)
-    lockphase.add_parameter('m_s', val=m_s, units='kg', static_target=True)
+    lockphase.add_parameter('a', val=a, units='m', static_target=True)
+    lockphase.add_parameter('b', val=b, units='m', static_target=True)
+    lockphase.add_parameter('mh', val=mh, units='kg', static_target=True)
+    lockphase.add_parameter('m', val=m, units='kg', static_target=True)
 
     # end contraints
     lockphase.add_boundary_constraint('x1', loc='final', equals=states_final['x1'], units='rad')
@@ -69,23 +62,18 @@ def main():
     lockphase.add_boundary_constraint('x2', loc='initial', equals=states_init['x2'], units='rad')
     lockphase.add_boundary_constraint('x4', loc='initial', equals=states_init['x4'], units='rad/s')
 
-    # add objective - TO DO - add cost function and obj
     lockphase.add_objective('cost')
-
 
     p.setup(check=True)
 
-    # initial guess / time - lockphase
     p.set_val('traj.lockphase.states:x1', lockphase.interp(ys=[states_init['x1'], states_final['x1']], nodes='state_input'), units='rad')
     p.set_val('traj.lockphase.states:x3', lockphase.interp(ys=[states_init['x3'], states_final['x3']], nodes='state_input'), units='rad/s')
     p.set_val('traj.lockphase.states:x2', lockphase.interp(ys=[states_init['x2'], states_final['x2']], nodes='state_input'), units='rad')
     p.set_val('traj.lockphase.states:x4', lockphase.interp(ys=[states_init['x4'], states_final['x4']], nodes='state_input'), units='rad/s')
     p.set_val('traj.lockphase.states:cost', lockphase.interp(xs=[0, 2, duration_lockphase], ys=[0, 50, 100], nodes='state_input'))
-    p.set_val('traj.lockphase.controls:tau', lockphase.interp(ys=[-10, 10], nodes='control_input'), units='N*m')
+    p.set_val('traj.lockphase.controls:tau', lockphase.interp(ys=[0, 10], nodes='control_input'), units='N*m')
 
-
-    # simulate and run problem
-    dm.run_problem(p, run_driver=True, simulate=True, simulate_kwargs={'method' : 'Radau', 'times_per_seg' : 10})
+    dm.run_problem(p, run_driver=True, simulate=True, simulate_kwargs={'method' : 'Radau', 'times_per_seg' : 3})
 
     #om.n2(p)
 
@@ -109,8 +97,10 @@ def main():
                   ('traj.lockphase.timeseries.time','traj.lockphase.timeseries.controls:tau','time','tau'),
                   ('traj.lockphase.timeseries.time', 'traj.lockphase.timeseries.states:cost', 'time', 'cost')],
                   title='Time History',p_sol=p,p_sim=sim_sol)
-    plt.savefig('openloop_locked_kneedwalker.pdf', bbox_inches='tight')
+    plt.savefig('compass_gait.pdf', bbox_inches='tight')
 
 if __name__ == '__main__':
     main()
+
+
 
